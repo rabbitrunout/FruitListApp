3665b0dff7cdcb8dc4a58feb91e907a36121242b
Irina Saf <irina.safronova0801@gmail.com>
2025-09-30 08:14:11 -0400

    Initial Commit

---
+++ FruitListApp/FruitListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitListAppApp.swift
+//  FruitListApp
+//
+//  Created by Irina Saf on 2025-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FruitListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
